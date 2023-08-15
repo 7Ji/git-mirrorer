1cfca6116a2a7a0872ec1111a154b1a14152fc5f
7Ji <pugokushin@gmail.com>
2023-08-15 14:57:38 +0800

    version.sh: use posix legacy redirect

--- version.sh
+++ version.sh
@@ -6,8 +6,8 @@ FIX=0
 if [ -e ".git" ]; then
     COMMIT_DATE=$(TZ=UTC git show -s --pretty=%cd --date=format-local:%Y%m%d HEAD 2>/dev/null)
     COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
-    git update-index --really-refresh &>/dev/null
-    if ! git diff-index --quiet HEAD &>/dev/null; then
+    git update-index --really-refresh >/dev/null 2>/dev/null
+    if ! git diff-index --quiet HEAD >/dev/null 2>/dev/null; then
         COMMIT_DIRTY='-dirty'
     fi
     COMMIT_INFO="-${COMMIT_DATE}.${COMMIT_HASH}${COMMIT_DIRTY}"
